1f4f5a6b5ab449817b11b48117d38c801729bfce
Simon Hryszko <simon@hryszko.dev>
2021-07-16 14:37:21 +0200

    Button's value from lang

--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -3,5 +3,12 @@
 return [
   'errorsTitle' => 'Holy guacamole!',
   'errorsDesc' => 'You should check in on some of those fields below.',
+  'close' => 'Close',
+  'add' => 'Add',
+  'employees' => 'Employees',
+  'managers' => 'Managers',
+  'departments' => 'Departments',
+  'titles' => 'Titles',
+  'salaries' => 'Salaries',
   '' => '',
 ];
--- resources/views/dashboard/common.blade.php
+++ resources/views/dashboard/common.blade.php
@@ -5,20 +5,20 @@
   <div class="container mt-5">
     <div class="row">
       <div class="col-12 col-md-3">
-        <a href="{{route('dashboard')}}">
-          <button type="button" class="btn {{$path=='employees' ? 'btn-success' : 'btn-outline-success'}} form-control mb-2">Employees</button>
+        <a href="{{route('employees')}}">
+          <button type="button" class="btn {{$path=='employees' ? 'btn-success' : 'btn-outline-success'}} form-control mb-2">{{__('common.employees')}}</button>
         </a>
         <a href="{{route('managers')}}">
-          <button type="button" class="btn {{$path=='managers' ? 'btn-danger' : 'btn-outline-danger'}} form-control mb-2">Managers</button>
+          <button type="button" class="btn {{$path=='managers' ? 'btn-danger' : 'btn-outline-danger'}} form-control mb-2">{{__('common.managers')}}</button>
         </a>
         <a href="{{route('departments')}}">
-          <button type="button" class="btn {{$path=='departments' ? 'btn-dark' : 'btn-outline-dark'}} form-control mb-2">Departments</button>
+          <button type="button" class="btn {{$path=='departments' ? 'btn-dark' : 'btn-outline-dark'}} form-control mb-2">{{__('common.departments')}}</button>
         </a>
         <a href="{{route('titles')}}">
-          <button type="button" class="btn {{$path=='titles' ? 'btn-info' : 'btn-outline-info'}} form-control mb-2">Titles</button>
+          <button type="button" class="btn {{$path=='titles' ? 'btn-info' : 'btn-outline-info'}} form-control mb-2">{{__('common.titles')}}</button>
         </a>
         <a href="{{route('salaries')}}">
-          <button type="button" class="btn {{$path=='salaries' ? 'btn-warning' : 'btn-outline-warning'}} form-control mb-2">Salaries</button>
+          <button type="button" class="btn {{$path=='salaries' ? 'btn-warning' : 'btn-outline-warning'}} form-control mb-2">{{__('common.salaries')}}</button>
         </a>
       </div>
       <div class="col-12 col-md-9">
