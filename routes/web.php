5348270bd2223c6b4c656a5b5753019693a9f60c
Simon Hryszko <simon@hryszko.dev>
2021-07-16 11:16:50 +0200

    New route POST/register

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\accessController;
-use App\Http\Controllers\authController;
+use App\Http\Controllers\UserController;
 
 
 Route::get('/', [accessController::class, 'login']);
 Route::get('register', [accessController::class, 'register']);
+
+Route::post('register', [UserController::class, 'create']);
