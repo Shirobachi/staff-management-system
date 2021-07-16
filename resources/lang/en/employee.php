8826ac8668bdf162d305b32736118d5599dd08fd
Simon Hryszko <simon@hryszko.dev>
2021-07-16 14:38:43 +0200

    Adding new records to employees table

---
+++ resources/lang/en/employee.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'new' => 'New employee',
+  'birthDate' => 'Birth date',
+  'firstName' => 'First name',
+  'lastName' => 'Last name',
+  'gender' => 'Gender',
+  'hireDate' => 'Hire date',
+  'male' => 'Male',
+  'female' => 'Female',
+  '' => '',
+];
--- resources/views/dashboard/employees.blade.php
+++ resources/views/dashboard/employees.blade.php
@@ -27,18 +27,42 @@
       </tr>
       @endforeach
       <tr>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
+        <td style="text-align: center;" colspan="6">
+          <i class="modalLink bi bi-plus-circle-dotted" data-bs-toggle="modal" data-bs-target="#newEmployee"></i>
+        </td>
       </tr>
     </tbody>
   </table>
+
+  <!-- Modal -->
+  <div class="modal fade" id="newEmployee" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="newEmployee">{{__('employee.new')}}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form id="add" method="POST" action="{{url()->current()}}/new">
+          @csrf
+          <input class="form-control mb-2" type="date" name="birthDate" placeholder="{{__('employee.birthDate')}}">
+          <input class="form-control mb-2" type="text" name="firstName" placeholder="{{__('employee.firstName')}}">
+          <input class="form-control mb-2" type="text" name="lastName" placeholder="{{__('employee.lastName')}}">
+          <select class="form-control mb-2" name="gender">
+            <option value="M">{{__('employee.male')}}</option>
+            <option value="F">{{__('employee.female')}}</option>
+          </select>
+          <input class="form-control mb-2" type="date" name="hireDate" placeholder="{{__('employee.hireDate')}}">
+        </form>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('common.close')}}</button>
+          <button form="add" type="submit" class="btn btn-primary">{{__('common.add')}}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 @endsection
