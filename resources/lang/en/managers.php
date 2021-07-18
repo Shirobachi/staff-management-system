7f70f6449705981be60164c6894af0437feabd02
Simon Hryszko <simon@hryszko.dev>
2021-07-18 20:53:06 +0200

    Edit manager langs

--- resources/lang/en/managers.php
+++ resources/lang/en/managers.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-  'deptNo' => 'Department number',
-  'empNo' => 'Employee number',
+  'deptNo' => 'Department',
+  'empNo' => 'Employee',
   'fromDate' => 'Date from',
   'toDate' => 'Date to',
   'new' => 'New manager',
   'now' => 'Still working', # if toDate if null mean if manager is still manager in this department
+  'edit' => 'Edit manager match ',
   '' => '',
 ];
