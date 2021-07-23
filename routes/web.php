a572f7391f0cdd88c635058096afaa57ca802985
Simon Hryszko <simon@hryszko.dev>
2021-07-23 20:58:46 +0200

    Changed routes (removed CRUD routes, only listings)

--- routes/web.php
+++ routes/web.php
@@ -24,42 +24,9 @@ Route::get('logout', function () {
   return view('auth.login', compact('info'));
 });
 
-
-Route::prefix('dashboard/employees')->group(function () {
-  Route::get('', [accessController::class, 'employees']) -> name('employees');  
-  Route::post('', [accessController::class, 'employeesFilter']);  
-  Route::post('new', [EmployeeController::class, 'create']);  
-  Route::get('delete/{id}', [EmployeeController::class, 'destroy']);  
-  Route::post('edit/{id}', [EmployeeController::class, 'edit']);  
-  Route::get('{mode}/{id}', [EmployeeController::class, 'index']);  
-});
-Route::prefix('dashboard/managers')->group(function () {
-  Route::get('', [accessController::class, 'managers']) -> name('managers');  
-  Route::post('new', [deptManagerController::class, 'create']);  
-  Route::get('delete/{id}', [deptManagerController::class, 'destroy']);  
-  Route::post('edit/{id}', [deptManagerController::class, 'edit']);  
-});
-Route::prefix('dashboard/departments')->group(function () {
-  Route::get('', [accessController::class, 'departments']) -> name('departments');  
-  Route::post('new', [DepartmentController::class, 'create']);  
-  Route::get('delete/{id}', [DepartmentController::class, 'destroy']);  
-  Route::post('edit/{id}', [DepartmentController::class, 'edit']);  
-});
-Route::prefix('dashboard/titles')->group(function () {
-  Route::get('', [accessController::class, 'titles']) -> name('titles');  
-  Route::post('new', [TitleController::class, 'create']);  
-  Route::get('delete/{title}/{date}', [TitleController::class, 'destroy']);  
-  Route::post('edit/{title}/{date}', [TitleController::class, 'edit']);  
-});
-Route::prefix('dashboard/salaries')->group(function () {
-  Route::get('', [accessController::class, 'salaries']) -> name('salaries');  
-  Route::post('new', [SalaryController::class, 'create']);  
-  Route::get('delete/{date}', [SalaryController::class, 'destroy']);  
-  Route::post('edit/{date}', [SalaryController::class, 'edit']);  
-});
-Route::prefix('dashboard/dept-emp')->group(function () {
-  Route::get('', [accessController::class, 'deptEmp']) -> name('dept-emp');  
-  Route::post('new', [DeptEmpController::class, 'create']);  
-  Route::get('delete/{date}', [DeptEmpController::class, 'destroy']);  
-  Route::post('edit/{date}', [DeptEmpController::class, 'edit']);  
-});
+Route::get('dashboard/employees', [accessController::class, 'employees']) -> name('employees');  
+Route::get('dashboard/deptManagers', [accessController::class, 'deptManagers']) -> name('deptManagers');  
+Route::get('dashboard/departments', [accessController::class, 'departments']) -> name('departments');  
+Route::get('dashboard/titles', [accessController::class, 'titles']) -> name('titles');  
+Route::get('dashboard/salaries', [accessController::class, 'salaries']) -> name('salaries');  
+Route::get('dashboard/deptEmp', [accessController::class, 'deptEmp']) -> name('deptEmp');  
