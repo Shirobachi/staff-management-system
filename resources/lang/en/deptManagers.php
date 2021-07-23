7c0b1656be396463b309be095b2bb744d53c006b
Simon Hryszko <simon@hryszko.dev>
2021-07-23 20:58:12 +0200

    Changed file name

--- resources/lang/en/deptManagers.php
+++ resources/lang/en/deptManagers.php
@@ -3,8 +3,13 @@
 return [
   'deptNo' => 'Department',
   'empNo' => 'Employee',
+
+  'firstName' => 'First name',
+  'lastName' => 'Surname',
+  'deptName' => 'Department',
   'fromDate' => 'Date from',
   'toDate' => 'Date to',
+  
   'new' => 'New manager',
   'now' => 'Still working', # if toDate if null mean if manager is still manager in this department
   'edit' => 'Edit manager',
