8c82fc5d9d0240d9d83b93d4bcc7ec711493fa32
Simon Hryszko <simon@hryszko.dev>
2021-07-24 20:56:55 +0200

    added langs

--- resources/lang/en/salaries.php
+++ resources/lang/en/salaries.php
@@ -6,6 +6,8 @@ return [
   'toDate' => 'Date to',
   'new' => 'New salary',
   'empNo' => 'Employee',
+  'firstName' => __('common.firstName'),
+  'lastName' => __('common.lastName'),
   'now' => 'Still apply',
   'edit' => 'Edit :name\'s salary',
   'delete' => 'Delete salary',
